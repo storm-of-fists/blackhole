b3581abe323b796665be94322c5e4420d38b32cc
ConnorJLatham <connor.lathamsec@gmail.com>
2023-01-21 10:19:28 +0000

    got sim working

--- python_solar_sim/sim/const.py
+++ python_solar_sim/sim/const.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 G = 6.67408e-11
 
-DIR_PATH = Path(__file__).parent.resolve()
+DIR_PATH = Path(__file__).parent.parent.resolve()
 YAML_PATH = DIR_PATH / 'instantiation_data'
 EPHEMERIS_PATH = DIR_PATH / 'instantiation_data' / 'de440.bsp'
