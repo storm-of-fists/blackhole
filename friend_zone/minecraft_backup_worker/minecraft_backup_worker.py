1d0afc8a0868a89879bfa56af93a7ac9f31bc32f
ConnorJLatham <connor.lathamsec@gmail.com>
2023-03-03 11:12:55 +0000

    making the backup worker better

--- friend_zone/minecraft_backup_worker/minecraft_backup_worker.py
+++ friend_zone/minecraft_backup_worker/minecraft_backup_worker.py
@@ -1,20 +1,17 @@
 # https://github.com/dropbox/dropbox-sdk-python/blob/master/example/back-up-and-restore/backup-and-restore-example.py
 # https://github.com/dropbox/dropbox-sdk-python/blob/master/example/updown.py
 
-import base.python as base
+from base.python import log
 import dropbox
 from dropbox.files import CommitInfo, WriteMode, UploadSessionCursor, DeleteArg
-from dropbox.exceptions import ApiError, AuthError
 from pathlib import Path
-import sys
 import shutil
-from progress.bar import IncrementalBar
 import schedule
 import time
 import datetime
 
 
-LOG = base.log.init("minecraft_backup_worker")
+LOG = log.init("minecraft_backup_worker")
 
 CREDS_PATH = Path("/credentials")
 
@@ -41,96 +38,117 @@ def refresh_token():
 
 
 DATA_PATH = Path("/server_data")
-
-SERVER_PATH = DATA_PATH / "test_dir"
+TMP_PATH = Path("/tmp")
 
 ARCHIVE_TYPE = "tar"
 ARCHIVE_NAME = "minecraft_server_backup"
 ARCHIVE_FILE_FULL = f"{ARCHIVE_NAME}.{ARCHIVE_TYPE}"
-ARCHIVE_PATH = DATA_PATH / ARCHIVE_FILE_FULL
+ARCHIVE_PATH = TMP_PATH / ARCHIVE_FILE_FULL
 
-DROPBOX_BACKUP_PATH = f"/{datetime.date.today()}_{ARCHIVE_NAME}.{ARCHIVE_TYPE}"
+DROPBOX_BACKUP_PATH = f"/{datetime.datetime.now()}_{ARCHIVE_NAME}.{ARCHIVE_TYPE}"
 
-MAX_CHUNK_SIZE = int(1e8) # 100 MB 1e7
+MAX_CHUNK_SIZE = int(1e8)  # 100 MB 1e7
 
-BACKUP_TIME_UTC = "09:00"
-CHECK_INTERVAL = 60 # seconds
+BACKUP_TIME_UTC = "10:00"  # 2am pst
+CHECK_INTERVAL = 60  # seconds
 DELETE_BACKUP_TIME = datetime.timedelta(days=3)
 
 
 def backup_archive(dbx):
-    LOG.info(f"Uploading {ARCHIVE_FILE_FULL} to Dropbox as {DROPBOX_BACKUP_PATH} ...")
+    try:
+        LOG.info(f"Uploading {ARCHIVE_FILE_FULL} to Dropbox as {DROPBOX_BACKUP_PATH} ...")
 
-    total_file_size = ARCHIVE_PATH.stat().st_size
-    LOG.info(f"File size to be uploaded: {total_file_size}")
-    
-    with open(ARCHIVE_PATH, 'rb') as f:
-        progress_bar = IncrementalBar('Upload Status', 
-                                    max=total_file_size/MAX_CHUNK_SIZE,
-                                    suffix='%(percent)d%%')
-
-        offset = 0
-        read_chunk = f.read(MAX_CHUNK_SIZE)
-        upload_session = dbx.files_upload_session_start(read_chunk)
-        offset += len(read_chunk)
-
-        while offset < total_file_size:
+        total_file_size = ARCHIVE_PATH.stat().st_size
+        LOG.info(f"File size to be uploaded: {total_file_size}")
+        
+        with open(ARCHIVE_PATH, 'rb') as f:
+            offset = 0
             read_chunk = f.read(MAX_CHUNK_SIZE)
-            dbx.files_upload_session_append_v2(
-                read_chunk,
-                UploadSessionCursor(session_id=upload_session.session_id, offset=offset),
-            )
+            LOG.info("Beginning upload session.")
+            upload_session = dbx.files_upload_session_start(read_chunk)
             offset += len(read_chunk)
 
-            progress_bar.next()
+            while offset < total_file_size:
+                LOG.info(f"Upload status: {((offset / total_file_size) * 100):.2f} %")
+                read_chunk = f.read(MAX_CHUNK_SIZE)
+                dbx.files_upload_session_append_v2(
+                    read_chunk,
+                    UploadSessionCursor(session_id=upload_session.session_id, offset=offset),
+                )
+                offset += len(read_chunk)
 
-        dbx.files_upload_session_finish(
-            read_chunk,
-            UploadSessionCursor(session_id=upload_session.session_id, offset=offset),
-            CommitInfo(path=DROPBOX_BACKUP_PATH, mode=WriteMode('overwrite'))
-        )
+            dbx.files_upload_session_finish(
+                read_chunk,
+                UploadSessionCursor(session_id=upload_session.session_id, offset=offset),
+                CommitInfo(path=DROPBOX_BACKUP_PATH, mode=WriteMode('overwrite'))
+            )
+
+            LOG.info("Upload session finished.")
+    except Exception as err:
+        LOG.error(err)
 
 
 def create_archive():
-    LOG.info(f"Creating archive at {ARCHIVE_PATH} from {SERVER_PATH}")
-    shutil.make_archive(BASE_PATH / ARCHIVE_NAME, ARCHIVE_TYPE, SERVER_PATH)
+    try:
+        LOG.info(f"Creating archive at {ARCHIVE_PATH} from {DATA_PATH}")
+        shutil.make_archive(f"{TMP_PATH}/{ARCHIVE_NAME}", ARCHIVE_TYPE, DATA_PATH)
+        LOG.info(f"Done creating archive.")
+    except Exception as err:
+        LOG.error(err)
 
 
 def delete_local_archive():
-    LOG.info(f"Removing local archive at {ARCHIVE_PATH}")
-    ARCHIVE_PATH.unlink()
+    try:
+        LOG.info(f"Removing local archive at {ARCHIVE_PATH}")
+        ARCHIVE_PATH.unlink()
+    except Exception as err:
+        LOG.error(err)
 
 
 def delete_old_files(dbx):
-    LOG.info("Checking files to delete.")
-    ids_to_delete = []
-    things_in_root = dbx.files_list_folder("")
+    try:
+        LOG.info("Checking files to delete.")
+        ids_to_delete = []
 
-    for thing in things_in_root.entries:
-        thing_id = thing.id
-        date_created = thing.server_modified
+        things_in_root = dbx.files_list_folder("")
 
-        if datetime.datetime.now() - date_created > DELETE_BACKUP_TIME:
-            LOG.info(f"Found file to delete: {thing.path_display}")
-            ids_to_delete.append(DeleteArg(thing_id))
+        for thing in things_in_root.entries:
+            thing_id = thing.id
+            date_created = thing.server_modified
 
-    if ids_to_delete:
-        LOG.info(f"Deleting these ids: {ids_to_delete}")
-        dbx.files_delete_batch(ids_to_delete)
-    else:
-        LOG.info("No files to delete.")
+            if datetime.datetime.now() - date_created > DELETE_BACKUP_TIME:
+                LOG.info(f"Found file to delete: {thing.path_display}")
+                ids_to_delete.append(DeleteArg(thing_id))
+
+        if ids_to_delete:
+            LOG.info(f"Deleting these ids: {ids_to_delete}")
+            dbx.files_delete_batch(ids_to_delete)
+            LOG.info("Done deleting files.")
+        else:
+            LOG.info("No files to delete.")
+    except Exception as err:
+        LOG.error(err)
 
 
 def perform_backup(dbx):
-    LOG.info("Start backup.")
+    try:
+        LOG.info("Start backup.")
 
-    dbx.refresh_access_token()
+        create_archive()
+        backup_archive(dbx)
+        delete_local_archive()
 
-    create_archive()
-    backup_archive(dbx)
-    delete_local_archive()
+        LOG.info("All done!")
+    except Exception as err:
+        LOG.error(err)
 
-    LOG.info("All done!")
+
+def refresh_dropbox_token():
+    try:
+        LOG.info("Refresh dropbox token.")
+        dbx.refresh_access_token()
+    except Exception as err:
+        LOG.error(err)
     
 
 if __name__ == "__main__":
@@ -141,6 +159,7 @@ if __name__ == "__main__":
         app_secret=app_secret()
     ) as dbx:
         LOG.info("Adding to scheduler.")
+        schedule.every().day.at(BACKUP_TIME_UTC).do(refresh_dropbox_token)
         schedule.every().day.at(BACKUP_TIME_UTC).do(perform_backup, dbx=dbx)
         schedule.every().day.at(BACKUP_TIME_UTC).do(delete_old_files, dbx=dbx)
 
