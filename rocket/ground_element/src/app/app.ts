26c5b8cd2761980e2ab81f0abb555db9225b740e
ConnorJLatham <connor.lathamsec@gmail.com>
2023-06-18 06:41:10 +0000

    d

--- rocket/ground_element/src/app/app.ts
+++ rocket/ground_element/src/app/app.ts
@@ -1,15 +1,16 @@
 import { BaseElement, html, customElement, property, css } from '../base/base.js';
+import '@material/web/iconbutton/standard-icon-button.js';
+import '@material/web/icon/icon.js';
+import '@material/web/linearprogress/linear-progress.js';
+import themes from '../../themes/themes.css' assert { type: 'css' };
 
 @customElement('app-home')
 export class App extends BaseElement {
-  static override styles = css`
+  static override styles = [themes, css`
     :host {
       display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
     }
-  `;
+  `];
 
   @property()
   name = 'World';
@@ -17,18 +18,25 @@ export class App extends BaseElement {
   @property({type: Number})
   count = 0;
 
+  @property({type: String, reflect: true})
+  class = "dark";
+
   override render() {
     return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Symbols+Outlined" rel="stylesheet">
+
+      <div>
+        <h1>${this.sayHello(this.name)}!</h1>
+        <md-standard-icon-button toggle><md-icon>check</md-icon></md-standard-icon-button>
+      </div>
       <slot></slot>
-      <img src=../../assets/test.jpg>
+      <img src=./assets/test.jpg>
+      
     `;
   }
 
   private _onClick() {
+    console.log(this.class)
     this.count++;
     this.dispatchEvent(new CustomEvent('count-changed'));
   }
