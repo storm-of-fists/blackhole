9d55fac93888c0b064a806502629041bb7ec1084
Connor Latham <connor.lathampri@gmail.com>
2023-12-24 00:42:55 -0800

    move more things

--- projects/dump/c_practice/ch3/state.c
+++
@@ -1,9 +0,0 @@
-typedef struct State {
-    float area;
-    float forest;
-    char name[10];
-} State;
-
-float get_percent(struct State *st) {
-    return 100 * (st->forest / st->area);
-}
--- projects/dump/c_practice/ch3/state.h
+++
@@ -1,7 +0,0 @@
-typedef struct State {
-    float area;
-    float forest;
-    char name[10];
-} State;
-
-float get_percent(struct State *st);
