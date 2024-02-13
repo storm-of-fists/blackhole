54ac171a6b511eaba4f28ec7ba413d3be16d293c
Connor Latham <connor.lathampri@gmail.com>
2024-02-12 22:49:36 -0800

    get htmx and go working

--- projects/blog/main.go
+++ projects/blog/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 )
+
+var COUNTER = 0
+
 // https://youtu.be/TT7SV-bAZyA
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("./templates/index.html"))
+		template := template.Must(template.ParseFiles("./static/index.html"))
 		template.Execute(w, nil)
 	})
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
 		template := template.Must(template.ParseFiles("./templates/post-1/index.html"))
-		// data := map[string][]Stock{
-		// 	"Results": SearchTicket(r.URL.Query.Get("key"))
-		// }
 		template.Execute(w, nil)
+	})
+
+	http.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.New("counter").Parse(fmt.Sprintf("<div id=\"counter\">%d</div>", COUNTER)))
+		template.Execute(w, nil)
+	})
+
+	http.HandleFunc("/decrease", func(w http.ResponseWriter, r *http.Request) {
+		COUNTER -= 1
+		w.Header().Add("Hx-Trigger", "counterChange")
+	})
+
+	http.HandleFunc("/increase", func(w http.ResponseWriter, r *http.Request) {
+		COUNTER += 1
+		w.Header().Add("Hx-Trigger", "counterChange")
 	})
 
 	log.Println("App running on 8000")
