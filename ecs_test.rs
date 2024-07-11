f0be6cb4e85aadb8aa21749f1fc2fba8c302ff3b
storm-of-fists <29338900+storm-of-fists@users.noreply.github.com>
2024-07-10 23:11:37 -0700

    Update ecs_test.rs
    using Rc and RefCell in order to perform composability.

--- ecs_test.rs
+++ ecs_test.rs
@@ -1,8 +1,8 @@
 use std::{
     time::{Duration, Instant, SystemTime, UNIX_EPOCH}, 
     collections::BTreeMap,
-    // rc::Rc,
-    // cell::RefCell,
+    rc::Rc,
+    cell::RefCell,
 };
 
 /// The period for the server logic to run.
@@ -15,13 +15,33 @@ pub struct Position {
     pub z: f32,
 }
 
-pub fn update_positions(
+#[derive(Debug)]
+pub struct Velocity {
+    pub vx: f32,
+    pub vy: f32,
+    pub vz: f32,
+}
+
+#[derive(Debug)]
+pub struct Mover {
+    pub position: Rc<RefCell<Position>>,
+    pub velocity: Rc<RefCell<Velocity>>,
+}
+
+pub fn update_movers(
     timing: &Timing,
-    positions: &mut BTreeMap<u64, Position>,
-    velocities: &BTreeMap<u64, Velocity>
+    movers: &BTreeMap<u64, Rc<RefCell<Mover>>>,
 ) {
-    for (id, velocity) in velocities.iter() {
-        let Some(position) = positions.get_mut(id) else {
+    for (_id, rc_mover) in movers.iter() {
+        let Ok(mover) = rc_mover.try_borrow() else {
+            continue;
+        };
+        
+        let Ok(mut position) = mover.position.try_borrow_mut() else {
+            continue;
+        };
+        
+        let Ok(velocity) = mover.velocity.try_borrow() else {
             continue;
         };
 
@@ -31,18 +51,6 @@ pub fn update_positions(
 
         // println!("position: {:?})", position);
     }
-}
-
-pub struct Velocity {
-    pub vx: f32,
-    pub vy: f32,
-    pub vz: f32,
-}
-
-pub struct Acceleration {
-    pub ax: f32,
-    pub ay: f32,
-    pub az: f32,
 }
 
 pub struct Timing {
@@ -69,7 +77,7 @@ pub fn update_rest_of_timing(
         std::thread::sleep(timing.step_overhead);
     }
     
-    timing.start_instant.elapsed() > Duration::from_millis(40)
+    timing.start_instant.elapsed() > Duration::from_millis(100000)
 }
 
 pub struct World {
@@ -77,9 +85,9 @@ pub struct World {
     
     timing: Timing,
 
-    positions: BTreeMap<u64, Position>,
-    velocities: BTreeMap<u64, Velocity>,
-    accelerations: BTreeMap<u64, Acceleration>,
+    positions: BTreeMap<u64, Rc<RefCell<Position>>>,
+    velocities: BTreeMap<u64, Rc<RefCell<Velocity>>>,
+    movers: BTreeMap<u64, Rc<RefCell<Mover>>>,
 }
 
 
@@ -97,10 +105,10 @@ pub fn main() {
         
         positions: BTreeMap::new(),
         velocities: BTreeMap::new(),
-        accelerations: BTreeMap::new(),
+        movers: BTreeMap::new(),
     };
     
-    for _ in 0..1000 {
+    for _ in 0..10000 {
         let entity_id = world.new_entity_id();
         world.register_mover(
             entity_id, 
@@ -129,7 +137,7 @@ impl World {
     pub fn remove_entity_by_id(&mut self, id: u64) {
         self.positions.remove(&id);
         self.velocities.remove(&id);
-        self.accelerations.remove(&id);
+        self.movers.remove(&id);
     }
     
     pub fn register_mover(
@@ -137,19 +145,44 @@ impl World {
         entity_id: u64,
         position: Position,
         velocity: Velocity
-    ) {
-        self.positions.insert(entity_id, position);
-        self.velocities.insert(entity_id, velocity);
+    ) -> Rc<RefCell<Mover>> {
+        let mover = Rc::new(RefCell::new(Mover {
+            position: self.register_position(entity_id, position),
+            velocity: self.register_velocity(entity_id, velocity),
+        }));
+        
+        self.movers.insert(entity_id, mover.clone());
+        
+        mover
+    }
+    
+    pub fn register_position(
+        &mut self,
+        entity_id: u64,
+        position: Position
+    ) -> Rc<RefCell<Position>> {
+        let rc_position = Rc::new(RefCell::new(position));
+        self.positions.insert(entity_id, rc_position.clone());
+        rc_position
+    }
+    
+    pub fn register_velocity(
+        &mut self,
+        entity_id: u64,
+        velocity: Velocity
+    ) -> Rc<RefCell<Velocity>> {
+        let rc_velocity = Rc::new(RefCell::new(velocity));
+        self.velocities.insert(entity_id, rc_velocity.clone());
+        rc_velocity
     }
     
     pub fn run(&mut self) {
         loop {
             update_timing_instant(&mut self.timing);
             
-            update_positions(
+            update_movers(
                 &self.timing, 
-                &mut self.positions, 
-                &self.velocities
+                &self.movers,
             );
             
             if update_rest_of_timing(&mut self.timing) {
