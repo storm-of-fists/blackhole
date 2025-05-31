e119532ddd76cc9cc849ba4d88ea378a84abfad3
connor <connor.lathampri@gmail.com>
2025-05-31 23:28:38 +0000

    some webcad stuff, going on hold!

---
+++ src/webcad/backend/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+Collection<Array<T>>
+Collection<Map<T>>
+
+Component is an element within a collection
+Components can be primitives or
+
+There are some highly connected pieces of data you want to have visibility into.
+Channel, can be local or remote
+
+Pooling Objects
+Generation Instances
+Channel -> Fault, Metric, Persistent
+Culling
+PmSets, some dynamic, others static. 
